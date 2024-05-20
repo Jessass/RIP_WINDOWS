9e876213c9669caa2d8ceb1427b61dfafab24e5b
tima <timahot@yandex.ru>
2024-05-20 16:31:12 +0300

    lastscript.bat

---
+++ lastscript.bat
@@ -0,0 +1,20 @@
+echo off
+md Hotarev
+cd Hotarev
+md Yurievich
+md Timophey
+pause
+ecgo>03072003.txt
+cd Timophey
+ecgo>3.txt
+pause
+cd ..
+cd ..
+del Hotarev /S /Q /F
+pause
+cd Hotarev
+rd Timophey
+rd Yurievich
+cd ..
+rd Hotarev
+pause
